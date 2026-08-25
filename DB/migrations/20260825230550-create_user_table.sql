9adda400c80887c48667288d470d6e16e9d60ade
mamepuro <beans.mamegame.laugh@icloud.com>
2026-08-25 23:53:36 +0900

    feat: #4 DB migrateの機構を実装

---
+++ DB/migrations/20260825230550-create_user_table.sql
@@ -0,0 +1,11 @@
+
+-- +migrate Up
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- +migrate Down
+DROP TABLE users;
